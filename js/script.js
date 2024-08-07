adb1d9749cb67382381b0284daf62bdb25554afb
Alan <alanzagi2007@gmail.com>
2024-08-07 20:56:53 +0700

    angka berjalan, rating, dst

---
+++ js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const counters = document.querySelectorAll(".counter");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const counter = entry.target;
+          const target = +counter.getAttribute("data-target");
+          const duration = 2000;
+          const start = 0;
+          let startTime = null;
+
+          const updateCounter = (currentTime) => {
+            if (!startTime) startTime = currentTime;
+            const elapsedTime = currentTime - startTime;
+            const progress = Math.min(elapsedTime / duration, 1);
+            counter.textContent = Math.floor(progress * target) + "+";
+            if (progress < 1) {
+              requestAnimationFrame(updateCounter);
+            } else {
+              counter.textContent = target + "+";
+            }
+          };
+
+          requestAnimationFrame(updateCounter);
+          observer.unobserve(counter);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  counters.forEach((counter) => observer.observe(counter));
+});
