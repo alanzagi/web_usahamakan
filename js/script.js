c74b0341592b0e006cab963c94473123098b83cf
Alan <alanzagi2007@gmail.com>
2024-08-15 20:26:36 +0700

    menambahkan form yang bisa menyimpan data

--- js/script.js
+++ js/script.js
@@ -35,3 +35,32 @@ document.addEventListener("DOMContentLoaded", function () {
   counters.forEach((counter) => observer.observe(counter));
 });
 // End Fungsi untuk penambahan angka
+
+document.getElementById("message-submit-button").addEventListener("click", function () {
+  const inputText = document.getElementById("message-input").value.trim(); // Menghapus spasi tambahan
+
+  // Memeriksa apakah input kosong
+  if (inputText === "") {
+    Swal.fire({
+      icon: "error",
+      title: "Gagal mengirim",
+      text: "Harap isi semua bidang sebelum mengirim pesan",
+      confirmButtonColor: "#f44336", // Ganti dengan warna yang diinginkan
+      confirmButtonText: "OK",
+    });
+    return; // Menghentikan eksekusi jika input kosong
+  }
+
+  const whatsappUrl = `https://wa.me/6281521550913?text=${encodeURIComponent(inputText)}`;
+  window.open(whatsappUrl, "_blank");
+});
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbxS0tNgVZ7qU1-STyikAmDrstz-mHtXRqKqZIPteTeMVBelTATd4PPd8q4IBkuZ3Si9/exec";
+const form = document.forms["database"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
